b161dbc4ed46e110c6368cbddc1717f5f8bd5742
= <=>
2023-11-10 12:39:30 +0800

    ✨ feat(修改格式):

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,36 +1,34 @@
 import { createApp, h } from "vue";
 import { RouterProvide } from "vue3-navigation";
-import  {createRouter,createWebHistory} from "vue-router"
+import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
 
-
 const crateRouter = createRouter({
-  history:createWebHistory(),
-  routes:[
+  history: createWebHistory(),
+  routes: [
     {
-      path:"/o1",
-      name:"o1",
-      component:()=>import("./o1.vue")
+      path: "/o1",
+      name: "o1",
+      component: () => import("./o1.vue"),
     },
     {
-      path:"/o2",
-      name:"o2",
-      component:()=>import("./o2.vue")
+      path: "/o2",
+      name: "o2",
+      component: () => import("./o2.vue"),
     },
     {
-      path:"/o3",
-      name:"o3",
-      component:()=>import("./o3.vue")
-    }
-  ]
-})
+      path: "/o3",
+      name: "o3",
+      component: () => import("./o3.vue"),
+    },
+  ],
+});
 
-const app =  createApp({
-  render() {
-    return h(RouterProvide, null, {
-      default:()=>h(App)
-    });
-  },
+const app = createApp({
+  render: () =>
+    h(RouterProvide, null, {
+      default: () => h(App),
+    }),
 });
 
 app.use(crateRouter);
