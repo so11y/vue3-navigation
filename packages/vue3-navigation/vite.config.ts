b44eb66cda688232ebfa40d18fcedde686e7bf0f
sorry <247076126@qq.com>
2023-11-12 17:03:35 +0800

    📃 docs(vite.build): 添加同步README

--- packages/vue3-navigation/vite.config.ts
+++ packages/vue3-navigation/vite.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, Plugin, searchForWorkspaceRoot } from "vite";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
+import { readFile, writeFile } from "fs/promises";
+import { existsSync } from "fs";
+
+function builderREADME(): Plugin {
+  return {
+    name: "builder-readme",
+    apply: "build",
+    async writeBundle(a, b) {
+      const __dirname = fileURLToPath(import.meta.url);
+      const root = searchForWorkspaceRoot(__dirname);
+      const packageREADMEPath = resolve(__dirname, "../README.md");
+      const RootREADME = await readFile(resolve(root, "./README.md"), "utf-8");
+      await writeFile(packageREADMEPath, RootREADME, "utf-8");
+    },
+  };
+}
 
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts(), builderREADME()],
   build: {
     lib: {
       name: "vue3Navigation",
